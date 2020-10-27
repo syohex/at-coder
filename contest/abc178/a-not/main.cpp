3e29264be30fdbfde302f85bc2c6226e964c7358
Shohei YOSHIDA <syohex@gmail.com>
2020-10-28 00:39:21 +0900

    Add ABC178 A

---
+++ contest/abc178/a-not/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int x;
+    std::cin >> x;
+    std::cout << (x == 0 ? 1 : 0) << std::endl;
+    return 0;
+}
---
+++ contest/abc178/a-not/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function test() {
+    input=$1
+    expected=$2
+
+    got=$(echo -n -e $input | ./a.out)
+    if [[ $got != $expected ]]; then
+        echo "Got: $got, Expected: $expected"
+        exit 1
+    fi
+}
+
+test 1 0
+test 0 1
+
