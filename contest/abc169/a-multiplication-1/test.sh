164e64dcfdcbd89b9f04badbf744a0122d97b19d
Shohei YOSHIDA <syohex@gmail.com>
2020-11-29 03:12:52 +0900

    Add ABC 169 a

---
+++ contest/abc169/a-multiplication-1/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function test() {
+    input=$1
+    expected=$2
+
+    got=$(echo -n -e $input | ./a.out)
+    if [[ $got != $expected ]]; then
+        echo "Got: $got, Expected: $expected"
+        exit 1
+    fi
+}
+
+test "2 5" 10
+test "100 100" 10000
+
