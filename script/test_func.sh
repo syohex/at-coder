bcd0c06dc67476918b4339c5bb80849a6ef9b38e
Shohei YOSHIDA <syohex@gmail.com>
2021-01-11 21:16:49 +0900

    Add helper script

---
+++ script/test_func.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function test() {
+    input=$1
+    expected=$2
+
+    exe=a.out
+    if [[ -e a.exe ]]; then
+        exe=a.exe
+    fi
+
+    got=$(echo -n -e $input | ./$exe)
+    if [[ $got != $expected ]]; then
+        echo "Got: $got, Expected: $expected"
+        exit 1
+    fi
+}
