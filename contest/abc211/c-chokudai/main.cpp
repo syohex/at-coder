dd8f2f3cb2f9f970bacec5103dab957f6a680069
Shohei YOSHIDA <syohex@gmail.com>
2021-08-07 22:08:27 +0900

    Reduce loop count and accepted

--- contest/abc211/c-chokudai/main.cpp
+++ contest/abc211/c-chokudai/main.cpp
@@ -11,17 +11,23 @@ int main() {
     std::string input("chokudai");
     constexpr long MOD = 1'000'000'007;
 
+    std::vector<std::vector<int>> indexes(8);
     for (int i = 0; i < 8; ++i) {
         for (int j = 0; j < len; ++j) {
             if (s[j] == input[i]) {
+                indexes[i].push_back(j);
                 if (i == 0) {
                     dp[j] = 1;
                 } else {
                     long tmp = 0;
-                    for (int k = 0; k < j; ++k) {
-                        if (s[k] == input[i - 1]) {
-                            tmp += dp[k] % MOD;
+                    int limit = indexes[i - 1].size();
+                    for (int k = 0; k < limit; ++k) {
+                        int index = indexes[i - 1][k];
+                        if (index >= j) {
+                            break;
                         }
+
+                        tmp += dp[indexes[i - 1][k]] % MOD;
                     }
 
                     dp[j] = tmp;
@@ -31,10 +37,8 @@ int main() {
     }
 
     long ret = 0;
-    for (int i = 0; i < len; ++i) {
-        if (s[i] == 'i') {
-            ret += dp[i] % MOD;
-        }
+    for (auto index : indexes.back()) {
+        ret += dp[index] % MOD;
     }
 
     std::cout << ret % MOD << std::endl;
