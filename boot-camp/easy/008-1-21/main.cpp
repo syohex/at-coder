384a8b9f0a61c6bdb40394e136ca64126ebb558d
Shohei YOSHIDA <syohex@gmail.com>
2021-09-04 17:21:15 +0900

    clang-format

--- boot-camp/easy/008-1-21/main.cpp
+++ boot-camp/easy/008-1-21/main.cpp
@@ -3,22 +3,22 @@
 #include <string>
 
 int main() {
-	int a, b;
-	std::cin >> a >> b;
+    int a, b;
+    std::cin >> a >> b;
 
-	int i = 1;
-	std::set<int> s;
-	while (true) {
-		int v = i * i;
-		if (v > 100100) {
-			break;
-		}
+    int i = 1;
+    std::set<int> s;
+    while (true) {
+        int v = i * i;
+        if (v > 100100) {
+            break;
+        }
 
-		s.insert(v);
-		++i;
-	}
+        s.insert(v);
+        ++i;
+    }
 
-	int n = std::stoi(std::to_string(a) + std::to_string(b));
-	std::cout << (s.find(n) != s.end() ? "Yes" : "No") << std::endl;
-	return 0;
+    int n = std::stoi(std::to_string(a) + std::to_string(b));
+    std::cout << (s.find(n) != s.end() ? "Yes" : "No") << std::endl;
+    return 0;
 }
