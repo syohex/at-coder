1a2d1c1da8e59f145740d0c609d1567d8418bc3e
Shohei YOSHIDA <syohex@gmail.com>
2021-09-04 16:44:03 +0900

    Add easy 008

---
+++ boot-camp/easy/008-1-21/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+int main() {
+	int a, b;
+	std::cin >> a >> b;
+
+	int i = 1;
+	std::set<int> s;
+	while (true) {
+		int v = i * i;
+		if (v > 100100) {
+			break;
+		}
+
+		s.insert(v);
+		++i;
+	}
+
+	int n = std::stoi(std::to_string(a) + std::to_string(b));
+	std::cout << (s.find(n) != s.end() ? "Yes" : "No") << std::endl;
+	return 0;
+}
---
+++ boot-camp/easy/008-1-21/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source ../../../script/test_func.sh
+
+test "1 21" Yes
+test "100 100" No
+test "12 10" No
